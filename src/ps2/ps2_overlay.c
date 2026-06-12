9b21fceaa1b5837e0a4991d1e9de3bc72bdb04dc
Un1q32 <joey.t.reinhart@gmail.com>
2026-06-11 22:51:40 -0400

    ok

--- src/ps2/ps2_overlay.c
+++ src/ps2/ps2_overlay.c
@@ -112,6 +112,8 @@ void PS2Overlay_setDebugOverlayState(DebugOverlayState state, Runner* runner) {
     Profiler_setEnabled(&runner->vmContext->profiler, PS2Overlay_getDebugOverlayState() == STATS_ENABLED_WITH_PROFILER);
     gOverlay.profilerFramesInWindow = 0;
     gOverlay.profilerOverlayText[0] = '\0';
+#else
+    (void)runner;
 #endif
 }
 
