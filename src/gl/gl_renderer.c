9c50c838546c540a39df55cc5039859d90d6d5ee
cobaltgit <cobaltsecuremail312@proton.me>
2026-07-08 12:13:00 +0100

    gl: remove redundant letterbox blit call in endFrameInit

--- src/gl/gl_renderer.c
+++ src/gl/gl_renderer.c
@@ -729,11 +729,6 @@ static void glEndFrameInit(Renderer* renderer) {
         glBindFramebuffer(GL_FRAMEBUFFER, gl->hostFramebuffer);
         return;
     }
-
-    if (gl->isGL3) {
-        int32_t appId = gl->base.runner->applicationSurfaceId;
-        GLCommon_beginLetterboxBlit(gl->surfaces[appId], gl->hostFramebuffer);
-    }
 }
 
 static void glEndFrameEnd(Renderer* renderer) {
