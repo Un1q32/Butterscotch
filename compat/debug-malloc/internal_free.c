4a7a7d7495f35d8a0f021aac56d2321660c332f8
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-12 15:20:12 -0400

    better

--- compat/debug-malloc/internal_free.c
+++ compat/debug-malloc/internal_free.c
@@ -1,15 +1,13 @@
-#include <errno.h>
-#include <stdio.h>
-#include <sys/mman.h>
+#include <stdlib.h>
+
+/* use the real system free */
+#undef free
 
 void __internal_free(void *ptr) {
   if (!ptr)
     return;
 
   ptr = (size_t *)ptr - 1;
-  size_t size = *(size_t *)ptr;
 
-  int err = errno;
-  munmap(ptr, size);
-  errno = err;
+  free(ptr);
 }
--- compat/debug-malloc/internal_malloc.c
+++ compat/debug-malloc/internal_malloc.c
@@ -1,14 +1,8 @@
 #include <errno.h>
-#include <sys/mman.h>
 #include <stdlib.h>
 
-#if defined(__linux__)
-#include <sys/user.h>
-#elif defined(_WIN32)
-#error windows not supported
-#else /* darwin and bsd, hope for the best */
-#include <sys/param.h>
-#endif
+/* use the real system malloc here */
+#undef malloc
 
 void *__internal_malloc(size_t size) {
   size += sizeof(size_t);
@@ -18,11 +12,8 @@ void *__internal_malloc(size_t size) {
     return NULL;
   }
   /* round up to multiple of page size */
-  if ((size & (PAGE_SIZE - 1)) != 0)
-    size = (size | (PAGE_SIZE - 1)) + 1;
-  size_t *ptr =
-      mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-  if (ptr == MAP_FAILED)
+  size_t *ptr = malloc(size);
+  if (!ptr)
     return NULL;
 
   ptr[0] = size;
